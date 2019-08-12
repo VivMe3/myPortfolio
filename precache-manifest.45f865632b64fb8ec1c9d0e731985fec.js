0b63e8dba668278a7edf33c6735806cad0beb306
Vivian Do <vivme3@gmail.com>
2019-08-12 15:50:29 -0400

    Updates

---
+++ precache-manifest.45f865632b64fb8ec1c9d0e731985fec.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4270022c60220878df1bed4dc26bc76e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fb510efb24515c25edb",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "27eafbf75721e2f6f1fc",
+    "url": "/static/css/main.676ecdeb.chunk.css"
+  },
+  {
+    "revision": "8fb510efb24515c25edb",
+    "url": "/static/js/2.19759a12.chunk.js"
+  },
+  {
+    "revision": "27eafbf75721e2f6f1fc",
+    "url": "/static/js/main.e7fcd6d5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6f8949118b142e864cebbe10229aeaf4",
+    "url": "/static/media/3days-donuts.6f894911.png"
+  },
+  {
+    "revision": "cd812d34ef912840c47ded5f25b6d6ff",
+    "url": "/static/media/bunny.cd812d34.jpg"
+  },
+  {
+    "revision": "cdb21b0dc60b184d573e9257626be2b4",
+    "url": "/static/media/chrysanthemum.cdb21b0d.JPG"
+  },
+  {
+    "revision": "0267fb619d7d894bfd5bccb3e2087b31",
+    "url": "/static/media/deadline.0267fb61.png"
+  },
+  {
+    "revision": "518404b45215b86bcea208f98f0943b3",
+    "url": "/static/media/digital.518404b4.png"
+  },
+  {
+    "revision": "0d84e37827f0a8dc1626270a74ff4a54",
+    "url": "/static/media/do_vivian.0d84e378.jpeg"
+  },
+  {
+    "revision": "80fe087772e2e390b871d9b10253fcaa",
+    "url": "/static/media/drips.80fe0877.jpg"
+  },
+  {
+    "revision": "b2685510b9b3a63f24f591348772ddd6",
+    "url": "/static/media/eeyore.b2685510.jpg"
+  },
+  {
+    "revision": "43b248a08f14521f209ef088aeb81da8",
+    "url": "/static/media/family-leave.43b248a0.png"
+  },
+  {
+    "revision": "381f1f8181e7d226830537af516c846b",
+    "url": "/static/media/graphic-design-poster.381f1f81.png"
+  },
+  {
+    "revision": "5af992891c05606e5f174d1f85d5b661",
+    "url": "/static/media/hand.5af99289.png"
+  },
+  {
+    "revision": "041dff7a96aba2186b0701aeaa4714a6",
+    "url": "/static/media/intro-wksp-sched.041dff7a.png"
+  },
+  {
+    "revision": "50baf05c17ecc28f9c52af4a675ee5e9",
+    "url": "/static/media/line1.50baf05c.jpg"
+  },
+  {
+    "revision": "237e80acfa00af97187ebd944ee30acf",
+    "url": "/static/media/line2.237e80ac.jpg"
+  },
+  {
+    "revision": "1a9187ed630f2e169ff1b81471491c16",
+    "url": "/static/media/line3.1a9187ed.jpg"
+  },
+  {
+    "revision": "ca3094707532c391b84c5a8bfa94bfae",
+    "url": "/static/media/mathematics-program-guide-2.ca309470.jpg"
+  },
+  {
+    "revision": "f5ef0aa9a0e16dde6b5487c2c7e59d04",
+    "url": "/static/media/mathematics-program-guide.f5ef0aa9.jpg"
+  },
+  {
+    "revision": "5e95d15eb45f25744432ea384dedb7de",
+    "url": "/static/media/mcf-events.5e95d15e.png"
+  },
+  {
+    "revision": "3b0e680def83476188dd3172345f683c",
+    "url": "/static/media/mothers-day.3b0e680d.png"
+  },
+  {
+    "revision": "8aa80c076b2e14b2ff2beab205955c2a",
+    "url": "/static/media/peach-mario.8aa80c07.jpg"
+  },
+  {
+    "revision": "ac28317e010fca40574d8427dde4cb7b",
+    "url": "/static/media/pigeon-adaptation.ac28317e.mp4"
+  },
+  {
+    "revision": "69618d7621a6fdfc0d64bd3058378bd7",
+    "url": "/static/media/play-pause.69618d76.png"
+  },
+  {
+    "revision": "20f774b76be9cb538a29cd140e8622fe",
+    "url": "/static/media/self-portrait.20f774b7.jpg"
+  },
+  {
+    "revision": "2a73e49b075fd3ee89e835d8fa8312d1",
+    "url": "/static/media/tech+X-sched.2a73e49b.png"
+  },
+  {
+    "revision": "d411b5c8988f289e54b595cd864c83f5",
+    "url": "/static/media/technica-circle.d411b5c8.png"
+  },
+  {
+    "revision": "abe7dc09ca4a5c29c20df2ffe32d3cc6",
+    "url": "/static/media/testudo.abe7dc09.JPG"
+  },
+  {
+    "revision": "297b4ca0a99ed8fe69b36a0c583abe1c",
+    "url": "/static/media/wedding-topper.297b4ca0.JPG"
+  },
+  {
+    "revision": "c76ecb8dcc99cca0f7c7f7232c0f3364",
+    "url": "/static/media/yoshi.c76ecb8d.jpg"
+  }
+]);
